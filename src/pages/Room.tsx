7b0e691a53e1cb0a19ff950113857f115bd00e54
jonathan.silva <jonathan.silva@elaw.com.br>
2021-09-27 17:09:51 -0300

    update css

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,10 +1,36 @@
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/button";
 
-export function Room ()  {
+import "../styles/room.scss"
+
+export function Room() {
   return (
     <>
-    <h1> sala 1 </h1>
+      <div id="page-room">
+        <header>
+          <div className="content">
+            <img src={logoImg} alt="letmeask" />
+            <div> codigo </div>
+          </div>
+        </header>
+        <main className="content">
+          <div className="room-title">
+            <h1>sala</h1>
+            <span className="bg"> 4 perguntas</span>
+          </div>
+          <form>
+            <textarea placeholder="o que voce quer perguntar ?" />
+            <div className="form-footer">
+              <span> Para enviar uma pergunta faça, <button>seu login </button> </span>
+              <Button type="submit">
+                Enviar pergunta
+              </Button>
+            </div>
+          </form>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
 export default Room;
