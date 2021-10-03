aac69c89fe2da5db52d29e4ac871a21a7bc353e1
jonathan.silva <jonathan.silva@elaw.com.br>
2021-10-03 12:47:22 -0300

    criação de tela de room para enviar pergunta

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import copyImg from "../assets/images/copy.svg";
+ 
+import "../styles/room-code.scss";
+
+type RoomCodeProps = {
+  code: string,
+
+}
+
+export function RoomCode( props: RoomCodeProps)  {
+
+  function copyRoomCodeToClipBoard(){
+    navigator.clipboard.writeText( props.code)
+
+  }
+
+  return (
+    <>
+      <button className="room-code" onClick={copyRoomCodeToClipBoard}>
+        <div>
+          <img src={copyImg} alt="Copy room code"></img>
+        </div>
+        <span> sala #{ props.code }</span>
+        
+      </button>
+    </>
+  );
+};
+
